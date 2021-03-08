85e69f5e3fe3c2a5053acb888c44bb8e2fc57da2
Samir Saadi <samirsaadi@gmail.com>
2021-03-08 20:54:13 -0300

    Definindo possíveis casos de teste

---
+++ test/OperacoesDeBoletoTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+
+class OperacoesDeBoletoTest {
+
+	void TesteFaturaPagaBoletos() {
+		Assertions.fail();
+	}
+
+	void TestaFaturaPagaMaiorBoletos() {
+		Assertions.fail();
+	}
+
+	void TestaFaturaPagaMenorBoletos() {
+		Assertions.fail();
+	}
+
+	void TestaTipoDePagamento() {
+		Assertions.fail();
+	}
+
+}
