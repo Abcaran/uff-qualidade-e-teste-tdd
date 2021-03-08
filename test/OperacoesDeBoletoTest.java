a8552b38268b991012ae17aa266163fa8240afaf
Samir Saadi <samirsaadi@gmail.com>
2021-03-08 20:55:58 -0300

    Add nomes para os testes

--- test/OperacoesDeBoletoTest.java
+++ test/OperacoesDeBoletoTest.java
@@ -1,19 +1,29 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class OperacoesDeBoletoTest {
 
+	@Test
+	@DisplayName("Teste para boletos com valor igual a fatura e pagos resultam em fatura paga")
 	void TesteFaturaPagaBoletos() {
 		Assertions.fail();
 	}
 
+	@Test
+	@DisplayName("Teste para boletos com valor superior a fatura e pagos resultam em fatura paga")
 	void TestaFaturaPagaMaiorBoletos() {
 		Assertions.fail();
 	}
 
+	@Test
+	@DisplayName("Teste para boletos com valor inferior a fatura e pagos resultam em fatura pendente")
 	void TestaFaturaPagaMenorBoletos() {
 		Assertions.fail();
 	}
-
+	
+	@Test
+	@DisplayName("Teste tipo de pagamento correto")
 	void TestaTipoDePagamento() {
 		Assertions.fail();
 	}
